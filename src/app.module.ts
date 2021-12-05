b87ceae763c977a6f57a1158a16b8a303a91d03e
Jose Bustillo <josebustillodealba@gmail.com>
2021-12-04 20:54:33 -0500

    feat: add lesson type module and resolver

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LessonModule } from './lesson/lesson.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: true
+    }),
+    LessonModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
